3b458cf983e56d9b692cb5c1ff12107040ad6a02
Harold Guzman <Gzmn.h10195@gmail.com>
2024-04-21 12:41:48 +0800

    Initial commit

---
+++ src/script/people.js
@@ -0,0 +1,162 @@
+let people =  [
+    {
+        "fullname": "Liam Salonga",
+        "workType": "UI Designer",
+        "email": "liamsal@gzmn.ai",
+        "tag": "UI",
+        "status": "81",
+        "task": "62"
+    },
+    {
+        "fullname": "Isabel Yap",
+        "workType": "Graphic Artist",
+        "email": "isabelyap@gzmn.ai",
+        "tag": "GRAPHIC ARTIST",
+        "status": "17",
+        "task": "40"
+    },
+    {
+        "fullname": "Ethan Dela Cruz",
+        "workType": "Web Developer",
+        "email": "ethandel@gzmn.ai",
+        "tag": "HTML",
+        "status": "53",
+        "task": "30"
+    },
+    {
+        "fullname": "Sophia Tan",
+        "workType": "UX Designer",
+        "email": "sophiatan@gzmn.ai",
+        "tag": "UI",
+        "status": "90",
+        "task": "10"
+    },
+    {
+        "fullname": "Lucas Santos",
+        "workType": "Admin",
+        "email": "lucas.santos@gzmn.ai",
+        "tag": "ADMIN",
+        "status": "74",
+        "task": "53"
+    },
+    {
+        "fullname": "Emma Cheng",
+        "workType": "Frontend Developer",
+        "email": "emma.cheng@gzmn.ai",
+        "tag": "HTML",
+        "status": "42",
+        "task": "7"
+    },
+    {
+        "fullname": "Noah Rivera",
+        "workType": "UI/UX Designer",
+        "email": "noahrivera@gzmn.ai",
+        "tag": "UI",
+        "status": "65",
+        "task": "22"
+    },
+    {
+        "fullname": "Ava Lee",
+        "workType": "Web Developer",
+        "email": "avalee@gzmn.ai",
+        "tag": "HTML",
+        "status": "23",
+        "task": "48"
+    },
+    {
+        "fullname": "Oliver Reyes",
+        "workType": "Graphic Artist",
+        "email": "oliverrey@gzmn.ai",
+        "tag": "GRAPHIC ARTIST",
+        "status": "86",
+        "task": "58"
+    },
+    {
+        "fullname": "Charlotte Lim",
+        "workType": "UI/UX Designer",
+        "email": "charlim@gzmn.ai",
+        "tag": "UX",
+        "status": "12",
+        "task": "5"
+    },
+    {
+        "fullname": "Mia Garcia",
+        "workType": "Web Developer",
+        "email": "miagarcia@gzmn.ai",
+        "tag": "AJAX",
+        "status": "33",
+        "task": "17"
+    },
+    {
+        "fullname": "Elijah Santos",
+        "workType": "Frontend Developer",
+        "email": "elijahsantos@gzmn.ai",
+        "tag": "HTML",
+        "status": "58",
+        "task": "28"
+    },
+    {
+        "fullname": "Amelia Tan",
+        "workType": "UI Designer",
+        "email": "ameliatan@gzmn.ai",
+        "tag": "UI",
+        "status": "95",
+        "task": "64"
+    },
+    {
+        "fullname": "Carter Chan",
+        "workType": "Admin",
+        "email": "carterchan@gzmn.ai",
+        "tag": "ADMIN",
+        "status": "41",
+        "task": "70"
+    },
+    {
+        "fullname": "Harper Reyes",
+        "workType": "Graphic Artist",
+        "email": "harperrey@gzmn.ai",
+        "tag": "GRAPHIC ARTIST",
+        "status": "29",
+        "task": "44"
+    },
+    {
+        "fullname": "James Garcia",
+        "workType": "Web Developer",
+        "email": "jamesgarc@gzmn.ai",
+        "tag": "AJAX",
+        "status": "60",
+        "task": "12"
+    },
+    {
+        "fullname": "Evelyn Cruz",
+        "workType": "UI/UX Designer",
+        "email": "evelyncruz@gzmn.ai",
+        "tag": "UI",
+        "status": "78",
+        "task": "35"
+    },
+    {
+        "fullname": "Logan Lim",
+        "workType": "Web Developer",
+        "email": "loganlim@gzmn.ai",
+        "tag": "AJAX",
+        "status": "50",
+        "task": "23"
+    },
+    {
+        "fullname": "Avery Santos",
+        "workType": "Graphic Artist",
+        "email": "averysantos@gzmn.ai",
+        "tag": "GRAPHIC ARTIST",
+        "status": "9",
+        "task": "41"
+    },
+    {
+        "fullname": "Ella Tan",
+        "workType": "UI Designer",
+        "email": "ellatan@gzmn.ai",
+        "tag": "UI",
+        "status": "88",
+        "task": "16"
+    }
+]
